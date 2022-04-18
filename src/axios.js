7942216db33113d4233643668d366dedc257b011
rahulbodara <rb.globalia@gmail.com>
2022-04-18 16:45:46 +0530

    test flow

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://product-testtask-backend-pkzefowlc-rahulbodara.vercel.app/api/api/';
+axios.defaults.baseURL = 'https://product-testtask-backend.vercel.app/api/api/';
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('token');
